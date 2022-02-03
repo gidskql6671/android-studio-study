ab9c173e981a5928e52ba76569a17324222b98ec
gidskql6671 <gidskql6671@gmail.com>
2022-02-03 22:42:13 +0900

    색깔 넣어보기

--- app/src/main/java/com/example/asstudy/MainActivity.kt
+++ app/src/main/java/com/example/asstudy/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.asstudy
 
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -21,24 +22,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
+        
+//        testAPI()
+        
+        testColor()
+    }
+    
+    private fun testColor() {
+        binding.img1.setColorFilter(Color.parseColor("#55ff00"))
+        binding.img2.setColorFilter(Color.parseColor("#2244aa"))
+    }
 
+    private fun testAPI() {
         binding.btnMessage.text = "안녕하세요"
         binding.btnMessage.setOnClickListener {
             val url = "http://118.41.31.14/forestpoint/test"
             val client = OkHttpClient()
             val request = Request.Builder().url(url).build()
-            
-            client.newCall(request).enqueue(object: Callback {
+
+            client.newCall(request).enqueue(object : Callback {
                 override fun onFailure(call: Call, e: IOException) {
                     e.printStackTrace()
                 }
 
                 override fun onResponse(call: Call, response: Response) {
-                    thread { 
+                    thread {
                         val str = response.body()?.string() ?: ""
-                        val res = Gson().fromJson<ForestPointResponse>(str, ForestPointResponse::class.java)
+                        val res = Gson().fromJson<ForestPointResponse>(
+                            str,
+                            ForestPointResponse::class.java
+                        )
                         println(res.data[0])
                     }
                 }
