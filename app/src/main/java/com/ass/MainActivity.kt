90855e0443186dbb623cdb2bc55c784db2e4dc23
gidskql6671 <gidskql6671@gmail.com>
2022-06-04 20:55:35 +0900

    Intent 웹페이지 연결

--- app/src/main/java/com/ass/MainActivity.kt
+++ app/src/main/java/com/ass/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.ass
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ass.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var mBinding : ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnNaver.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.naver.com"))
+            startActivity(intent)
+        }
     }
 }
