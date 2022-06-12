6e87ee230c82a32ecef3ad07058d1d9d821ebcc8
gidskql6671 <gidskql6671@gmail.com>
2022-06-12 19:56:12 +0900

    타이머 실습

--- app/src/main/java/com/ass/MainActivity.kt
+++ app/src/main/java/com/ass/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.ass
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.util.Log
-import android.view.KeyEvent
+import android.os.SystemClock
 import androidx.appcompat.app.AppCompatActivity
 import com.ass.databinding.ActivityMainBinding
 
@@ -12,37 +9,39 @@ import com.ass.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
+    private var pauseTime = 0L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(binding.root)
 
-        binding.editText.addTextChangedListener(object: TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        binding.btnStart.setOnClickListener {
+            binding.chronometer.base = SystemClock.elapsedRealtime() + pauseTime
+            binding.chronometer.start()
 
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                TODO("Not yet implemented")
-            }
-
-            override fun afterTextChanged(p0: Editable?) {
-            }
-        })
-    }
-
-    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        when(keyCode) {
-            KeyEvent.KEYCODE_BACK -> Log.d("ass", "뒤로가기 버튼")
-            KeyEvent.KEYCODE_VOLUME_UP -> Log.d("ass", "볼륨 업 버튼")
-            KeyEvent.KEYCODE_VOLUME_DOWN -> Log.d("ass", "볼륨 다운 버튼")
+            binding.btnStop.isEnabled = true
+            binding.btnReset.isEnabled = true
+            it.isEnabled = false
         }
 
-        return super.onKeyDown(keyCode, event)
-    }
+        binding.btnStop.setOnClickListener {
+            pauseTime = binding.chronometer.base - SystemClock.elapsedRealtime()
+            binding.chronometer.stop()
 
-    override fun onBackPressed() {
-        Log.d("ass", "뒤로가기 버튼")
-        super.onBackPressed()
+            binding.btnStart.isEnabled = true
+            binding.btnReset.isEnabled = true
+            it.isEnabled = false
+        }
+
+        binding.btnReset.setOnClickListener {
+            pauseTime = 0L
+            binding.chronometer.base = SystemClock.elapsedRealtime()
+            binding.chronometer.stop()
+
+            binding.btnStart.isEnabled = true
+            binding.btnStop.isEnabled = false
+            it.isEnabled = false
+        }
     }
 }
