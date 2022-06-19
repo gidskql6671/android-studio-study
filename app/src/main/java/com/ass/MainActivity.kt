aa1657dd4f3211d0dfc4ab663203fd83e32a1080
gidskql6671 <gidskql6671@gmail.com>
2022-06-19 15:07:55 +0900

    Toolbar
    다양한 기능 및 레이아웃을 위해 ActionBar 대신 Toolbar를 사용할 수 있다.

--- app/src/main/java/com/ass/MainActivity.kt
+++ app/src/main/java/com/ass/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
 
         binding.navigateMessengerIntro.setOnClickListener {
             val intent = Intent(this, MessengerIntroActivity::class.java)
